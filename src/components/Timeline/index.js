fca83fbe2d98579ba1edda5b95cd36bb67bcf189
Tânia Rocha <taniapassosrocha@gmail.com>
2020-11-25 17:12:46 +0000

    Add more company icons

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types"
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import SchoolIcon from "@material-ui/icons/School";
-import WorkIcon from "@material-ui/icons/Work";
 import StarIcon from "@material-ui/icons/Star";
 import Img from "gatsby-image/withIEPolyfill"
 import 'react-vertical-timeline-component/style.min.css';
@@ -18,8 +16,8 @@ const Timeline = ({data}) => {
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="December 2018 to present"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<WorkIcon />}
+          iconClassName='aurora-icon'
+          icon={<Img alt="aurora" fluid={data.aurora.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Software developer</h3>
           <h4 className="vertical-timeline-element-subtitle">Aurora digital</h4>
@@ -30,61 +28,60 @@ const Timeline = ({data}) => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="May 2017 to December 2018"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
+          iconClassName='oncostats-icon'
+          icon={<Img alt="oncostats" fluid={data.oncostats.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Software developer</h3>
           <h4 className="vertical-timeline-element-subtitle">Oncostats</h4>
           <p>
-          Oncostats software development
-
+            Oncostats software development
           </p>
           <p>
-  Product definition with the scientific team
-
+            Product definition with the scientific team
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="October 2015 to May 2016"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
+          iconClassName='coupzon-icon'
+          icon={<Img alt="coupzon" fluid={data.coupzon.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Freelance web developer</h3>
           <h4 className="vertical-timeline-element-subtitle">Coupzon</h4>
           <p>
-          Mobile application development (PhoneGap)
+            Mobile application development (PhoneGap)
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="April 2014 to May 2015"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
+          iconClassName='buu-icon'
+          icon={<Img alt="buu" fluid={data.buu.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Freelance flash developer</h3>
           <h4 className="vertical-timeline-element-subtitle">BUU design</h4>
           <p>
-          Corporate TV content development
+            Corporate TV content development
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="March 2013 to April 2017"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<WorkIcon />}
+          iconStyle={{ background: '#db001b' }}
+          iconClassName='muzzley-icon'
+          icon={<Img alt="muzzley" fluid={data.muzzley.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Front-End developer</h3>
           <h4 className="vertical-timeline-element-subtitle">Muzzley</h4>
           <p>
-          Development of mobile web interfaces to communicate with IoT devices
+            Development of mobile web interfaces to communicate with IoT devices
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="April 2008 to February 2013"
           iconClassName='mobbit-icon'
-          icon={<Img alt="one" fluid={data.mobbit.image.fluid} />}
+          icon={<Img alt="mobbit" fluid={data.mobbit.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Multimedia developer</h3>
           <h4 className="vertical-timeline-element-subtitle">Mobbit Systems</h4>
@@ -95,8 +92,8 @@ const Timeline = ({data}) => {
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2004 - 2008"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
+          iconClassName='ipvc-icon'
+          icon={<Img alt="ipvc" fluid={data.ipvc.image.fluid} />}
         >
           <h3 className="vertical-timeline-element-title">Computer Graphics and Multimedia Engineering</h3>
           <h4 className="vertical-timeline-element-subtitle">ESTG – Polytechnic Institute of Viana do Castelo</h4>
@@ -112,6 +109,31 @@ const Timeline = ({data}) => {
 Timeline.propTypes = {
   data: PropTypes.shape({
     mobbit: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+    muzzley: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+    buu: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+    coupzon: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+    oncostats: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+    ipvc: PropTypes.shape({
       image: PropTypes.shape({
         fluid: PropTypes.shape({}).isRequired,
       }).isRequired,
--- src/components/Timeline/withQuery.js
+++ src/components/Timeline/withQuery.js
@@ -5,7 +5,54 @@ const query = graphql`
   query {
     mobbit: file(relativePath: { eq: "icon-mobbit.jpg" }) {
       image: childImageSharp {
-        fluid(maxWidth: 1000, quality: 85) {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    muzzley: file(relativePath: { eq: "muzzley-icon.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    buu: file(relativePath: { eq: "buu.jpg" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    coupzon: file(relativePath: { eq: "coupzon.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    oncostats: file(relativePath: { eq: "oncostats.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    aurora: file(relativePath: { eq: "aurora.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    ipvc: file(relativePath: { eq: "ipvc.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 100, quality: 100) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
